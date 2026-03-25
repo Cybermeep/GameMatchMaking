407bd756db5f97039dac34d885bc09499cbb842a
Phillip <phillipgithub@gmail.com>
2026-03-25 17:49:47 -0500

    updates

--- GroupSession.java
+++ GroupSession.java
@@ -1,8 +1,7 @@
 public class GroupSession
 {
     private int sessionID;
-    private Group group
-    private Game game
-    private DateTime scheduledDate
-    private int duration
+    private Group group;
+    private DateTime scheduledDate;
+    private int duration;
 }
